83768e6eafb6980accf3721dc20a02461fe11caf
Vyacheslav0000 <55128388+Vyacheslav0000@users.noreply.github.com>
2020-07-10 00:20:05 +0300

    дз-7

---
+++ Plate.java
@@ -0,0 +1,28 @@
+package ru.geerbrains;
+
+public class Plate {
+   public int amountOfFood;
+
+    Plate(int amountOfFood) {
+        this.amountOfFood = amountOfFood;
+    }
+
+    @Override
+    public String toString() {
+        return "Тарелка: " + amountOfFood;
+    }
+
+    void decreaseFood(int appetite) {
+        if (amountOfFood >= appetite) {
+            amountOfFood -= appetite;
+        }
+    }
+
+    int getAmountOfFood() {
+        return amountOfFood;
+    }
+
+    void increaseFood(int amount) {
+        amountOfFood += amount;
+    }
+}
